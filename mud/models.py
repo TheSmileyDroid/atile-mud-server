1713129b9e24b5dd48deb1f24ecbd57b5ad8e5e3
TheSmileyDroid <ghsilha@gmail.com>
2021-10-05 22:25:03 -0300

    password size

--- mud/models.py
+++ mud/models.py
@@ -3,7 +3,7 @@ from atile import db, ma
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
     def __init__(self, email, password):
         self.email = email
