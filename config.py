718269096dc15ad8bb7e43f9e11375e7d2f534e7
TheSmileyDroid <ghsilha@gmail.com>
2021-10-05 22:02:18 -0300

    Database

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ import random
 random_str = string.ascii_letters + string.digits + string.ascii_uppercase
 key = ''.join(random.choice(random_str) for i in range(12))
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///atile.db')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///atile.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = key
